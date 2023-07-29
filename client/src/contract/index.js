0c6e69f997bc83e9c94d188eeb8aa4ba55c1dc91
addydist <adityapaljune@gmail.com>
2023-07-29 20:24:07 +0530

    first commit

---
+++ client/src/contract/index.js
@@ -0,0 +1,3 @@
+import contract from './AVAXGods.json'
+export const ADDRESS='0x53C4C11c1e3378327ADcd640fBeE38280B30Af47'
+export const { abi : ABI }=contract;
---
+++ client/src/page/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import CreateBattle from "./CreateBattle";
+import JoinBattle from "./JoinBattle";
+import Battle from "./Battle";
+import Battleground from "./BattleGound";
+export{
+    Home,
+    CreateBattle,
+    JoinBattle,
+    Battle,
+    Battleground
+}
